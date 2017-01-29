fcefc64f6dc9abb81cbc431d0c94f62bd7f5897f
mnichangxin <mnichangxin@163.com>
2017-01-29 18:06:21 +0800

    commit

--- src/com/nowcoder/sort/HeapSort.java
+++ src/com/nowcoder/sort/HeapSort.java
@@ -1,59 +1,63 @@
 /* 堆排序 */
 package com.nowcoder.sort;
 
-public class HeapSort {
-	//建堆
-	public int[] buildMaxHap(int[] A) {
-		for (int i = A.length / 2; i >= 0; i--) {
-			adjustHeap(A, i, A.length);
+public class HeapSort {	
+	//堆的调整
+	public void heapAdjust(int[] A, int parent, int n) {
+		int temp = A[parent]; //保存当前父结点
+		int child = 2 * parent + 1; //获得左孩子
+		
+		while (child < n) {
+			//如果右孩子结点值大于左孩子结点值，则选取右孩子结点
+			if (child + 1 < n && A[child] < A[child + 1]) {
+				child++;
+			}
+			//如果父结点值大于孩子结点的值，则直接结束
+			if (temp >= A[child]) {
+				break;
+			}
+			
+			A[parent] = A[child]; //把孩子结点的值赋给父结点
+			
+			parent = child;
+			child = 2 * parent + 1;
+		}
+		
+		A[parent] = temp;
+	}
+	
+	//堆的建立
+	public int[] buildMaxHeap(int[] A, int n) {
+		//循环建立初始堆
+		for (int i = n / 2; i >= 0; i--) {
+			heapAdjust(A, i, n - 1);
+		}
+		
+		//进行n-1次循环，完成排序
+		for (int i = n - 1; i > 0; i--) {
+			//最后一个元素和第一元素进行交换
+			int temp = A[i];
+			A[i] = A[0];
+			A[0] = temp;
+		
+			//筛选 R[0]结点，得到i-1个结点的堆
+			heapAdjust(A, 0, i);
 		}
 		
 		return A;
-	}
-	
-	//堆的调整：将元素A[k]自下往上逐步调整树形结构
-	public void adjustHeap(int[] A, int k, int n) {
-		int temp = A[k];
-		
-		for (int i = 2 * k + 1; i < n; i = 2 * i + 1) {
-			if (i != n - 1 && A[i] < A[i + 1]) {
-				i++; //右 -> 左，取右
-			}
-			if (temp > A[i]) {
-				break; //根 -> 右，结束
-			} else {
-				A[k] = A[i]; //改变根植，并继续向下调整
-				k = i; //修改k值，以便继续向下调整
-			}
-		}
-		
-		A[k] = temp; //被调整的结点的值放入最终位置
 	}
 	
 	public int[] heapSort(int[] A, int n) {
-		//建立大根堆
-		A = buildMaxHap(A);
-		
-		//调整排序
-		for (int i = n - 1; i > 0; i--) {
-			//堆顶和堆底元素交换
-			int temp = A[0];
-			A[0] = A[i];
-			A[i] = temp;
-			adjustHeap(A, 0, i); //将剩余的元素整理成堆
-		}
-		
-		return A;
+		return buildMaxHeap(A, n);
 	}
 	
 	public static void main(String[] args) {
-		int[] arr = {32,103,24,88,95,70,97,15,102,6,79,46,51,37,93,108,9,58,53,58,79,36,58,91,78,58,61,81};
-	
-		int[] arrPrint = new HeapSort().heapSort(arr, arr.length);
+		int[] arr = {11, 12, 1, 43, 5, 7, 32, 68, 86, 12, 53, 65, 27, 43};
 		
-		for (int i = 0; i < arrPrint.length; i++) {
-			System.out.println(arrPrint[i]);
+		int[] printArr = new HeapSort().heapSort(arr, arr.length);
+		
+		for (int i = 0; i < printArr.length; i++) {
+			System.out.println(printArr[i]);
 		}
 	}
 }
- 
