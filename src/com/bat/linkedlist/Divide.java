658248865c8a218bedaae3c806fa279b9f6aec11
mnichangxin <mnichangxin@163.com>
2017-03-06 22:47:44 +0800

    commit

--- src/com/bat/linkedlist/Divide.java
+++ src/com/bat/linkedlist/Divide.java
@@ -1,6 +1,5 @@
+/* 链表的分化 */
 package com.bat.linkedlist;
-
-import javax.xml.soap.Node;
 
 /*
 class ListNode {
@@ -15,21 +14,42 @@ class ListNode {
 
 public class Divide {
 	public ListNode listDivide(ListNode head, int val) {
-		if (head == null) {
-			return null;
-		}
-		
-		ListNode p = head;
-		
-		while (p!= null) {
-			if (p.val < val) {
-				 
-			} else if (p.val > val) {
-				
-			} else {
-				
-			}
-		}
+		ListNode sH = null;
+        ListNode sT = null;
+        ListNode bH = null;
+        ListNode bT = null;
+        ListNode next = null;
+        
+        while (head != null) {
+            next = head.next;
+            head.next = null;
+            
+            if (head.val <= val) {
+                if (sH == null) {
+                    sH = head;
+                    sT = head;
+                } else {
+                    sT.next = head;
+                    sT = head;
+                }
+            } else {
+                if (bH == null) {
+                    bH = head;
+                    bT = head;
+                } else {
+                    bT.next = head;
+                    bT = head;
+                }
+            }
+            
+            head = next;
+        }
+        
+        if (sT != null) {
+            sT.next = bH;
+        }
+        
+        return sH != null ? sH : bH;
 	}
 	
 	public static void main(String[] args) {
