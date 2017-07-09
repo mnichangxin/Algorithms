9f2b02954e958a50d4d455f9616eb48b68d48549
星竹工作室 <mazk@loverqi.cn>
2017-07-09 18:15:17 +0800

    commit

--- src/com/classic/DFS.java
+++ src/com/classic/DFS.java
@@ -2,148 +2,132 @@
 package com.classic;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Stack;
 
 /* 邻接矩阵模型类 */
 class AMWGraph {
-	private ArrayList vertexList; // 存储点的链表
-	private int[][] edges; // 邻接矩阵，用于存储边
-	private int numOfEdges; // 边的数目
-	private boolean[] isVisited;
+	private String[] vertices; // 存储结点的信息
+	private int[][] arcs; // 邻接矩阵
+	private int vexnum; // 结点的数目
+	private boolean[] isVisited; // 存储结点是否被访问
 	
-	// 初始化矩阵
 	public AMWGraph(int n) {
-		edges = new int[n][n];
-		vertexList = new ArrayList(n);
-		numOfEdges = 0;
+		this.vexnum = n;
+		this.arcs = new int[n][n];
+		this.isVisited = new boolean[n];
 		
-		isVisited = new boolean[n];
+		for (int i = 0; i < arcs.length; i++) {
+			for (int j = 0; j < arcs[i].length; j++) {
+				arcs[i][j] = 0;
+			}
+		}
 		
 		for (int i = 0; i < n; i++) {
 			this.isVisited[i] = false;
 		}
 	}
 	
-	// 得到结点的个数
-	public int getNumOfVertex() {
-		return vertexList.size();
+	public void setVertices(String[] vertices) {
+		this.vertices = vertices;
 	}
 	
-	public int getNumOfEdges() {
-		return numOfEdges;
+	public void setArcs(int i, int j) {
+		this.arcs[i][j] = 1;
 	}
 	
-	// 得到结点i的数据
-	public Object getValueByIndex(int i) {
-		return vertexList.get(i);
-	}
-	
-	// 返回v1, v2的权值
-	public int getWeight(int v1, int v2) {
-		return edges[v1][v2];
-	}
-	
-	// 插入结点
-	public void insertVertex(Object vertex) {
-		vertexList.add(vertexList.size(), vertex);
-	}
-	
-	// 插入结点
-	public void insertEdge(int v1, int v2, int weight) {
-		edges[v1][v2] = weight;
-		numOfEdges++;
-	}
-	
-	// 删除结点
-	public void deleteEdge(int v1, int v2) {
-		edges[v1][v2] = 0;
-		numOfEdges--;
-	}
-	
-	// 得到第一个邻接结点的下标
-	public int getFirstNeighbor(int index) {
-		for (int j = 0; j < vertexList.size(); j++) {
-			if (edges[index][j] > 0) {
-				return j;
+	public void traverse(int i) {
+		this.isVisited[i] = true;
+		
+		System.out.print(this.vertices[i] + " ");
+		
+		for (int j = 0; j < this.vexnum; j++) {
+			if (this.arcs[i][j] == 1 && this.isVisited[j] == false) {
+				traverse(j);
 			}
 		}
-		
-		return -1;
 	}
 	
-	// 根据前一个邻接结点的下标来取得下一个邻接结点
-	public int getNextNeigbor(int v1, int v2) {
-		for (int j = v2 + 1; j < vertexList.size(); j++) {
-			if (edges[v1][j] > 0) {
-				return j;
+	// 深度优先遍历（递归）
+	public void DFSTraverse() {
+		for (int i = 0; i < this.vexnum; i++) {
+			if (this.isVisited[i] == false) {
+				this.traverse(i);
 			}
 		}
-		
-		return -1;
 	}
+	
+	// 深度优先遍历（非递归）
+	public void DFS() {
+		Stack<Integer> s = new Stack<Integer>();
+        
+		for (int i = 0; i < vexnum; i++) {
+            if (!this.isVisited[i]) {
+                //连通子图起始节点
+                s.add(i);
+                do{ 
+                    // 出栈
+                    int curr = s.pop();
 
-	// 私有函数，深度优先遍历
-	private void depthFirstSearch(boolean[] isVisited, int i) {
-		System.out.print(this.getValueByIndex(i) + " ");
-		
-		isVisited[i] = true; // 置该结点为访问
-		
-		int w = this.getFirstNeighbor(i);
-		
-		while (w != -1) {
-			if (!isVisited[w]) {
-				this.depthFirstSearch(isVisited, w);
-			}
-			
-			w = this.getNextNeigbor(i, w);
-		}
-	}
-	
-	// 对外公开函数，深度优先遍历
-	public void depthFirstSearch() {
-		for (int i = 0; i < this.getNumOfVertex(); i++) {
-			// 对于非连通图，并不是通过一个节点就一定可以遍历所有结点的
-			if (!isVisited[i]) {
-				this.depthFirstSearch(isVisited, i);
-			}
-		}
+                    // 如果该节点还没有被遍历，则遍历该节点并将子节点入栈
+                    if(this.isVisited[curr]==false) {
+                        // 遍历并打印
+                        this.traverse(curr);
+                        this.isVisited[curr] = true;
+
+                        // 没遍历的子节点入栈
+                        for (int j = vexnum - 1; j >= 0; j--) {
+                            if (this.arcs[curr][j] == 1 && this.isVisited[j] == false) {
+                                s.add(j);
+                            }
+                        }
+                    }
+                } while(!s.isEmpty());
+            }
+        }
 	}
 }
 
+/* 主类  */
 public class DFS {
 	public static void main(String[] args) {
-		int n = 8, e = 8;
-		String[] labels = {"1", "2", "3", "4", "5", "6", "7", "8"};
+		AMWGraph g = new AMWGraph(9);
 		
-		AMWGraph graph = new AMWGraph(n);
-		
-		// 插入结点
-		for (String label : labels) {
-			graph.insertVertex(label);
-		}
-		
-		// 插入九条边
-		graph.insertEdge(0, 1, 1);
-        graph.insertEdge(0, 2, 1);
-        graph.insertEdge(1, 3, 1);
-        graph.insertEdge(1, 4, 1);
-        graph.insertEdge(3, 7, 1);
-        graph.insertEdge(4, 7, 1);
-        graph.insertEdge(2, 5, 1);
-        graph.insertEdge(2, 6, 1);
-        graph.insertEdge(5, 6, 1);
-        graph.insertEdge(1, 0, 1);
-        graph.insertEdge(2, 0, 1);
-        graph.insertEdge(3, 1, 1);
-        graph.insertEdge(4, 1, 1);
-        graph.insertEdge(7, 3, 1);
-        graph.insertEdge(7, 4, 1);
-        graph.insertEdge(6, 2, 1);
-        graph.insertEdge(5, 2, 1);
-        graph.insertEdge(6, 5, 1);
-		
-		System.out.println("深度优先搜索序列为：");
-		graph.depthFirstSearch();
-		System.out.println();
+        String[] vertices = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+ 
+        g.setVertices(vertices);
+
+        g.setArcs(0, 1);
+        g.setArcs(0, 5);
+        g.setArcs(1, 0);
+        g.setArcs(1, 2);
+        g.setArcs(1, 6);
+        g.setArcs(1, 8);
+        g.setArcs(2, 1);
+        g.setArcs(2, 3);
+        g.setArcs(2, 8);
+        g.setArcs(3, 2);
+        g.setArcs(3, 4);
+        g.setArcs(3, 6);
+        g.setArcs(3, 7);
+        g.setArcs(3, 8);
+        g.setArcs(4, 3);
+        g.setArcs(4, 5);
+        g.setArcs(4, 7);
+        g.setArcs(5, 0);
+        g.setArcs(5, 4);
+        g.setArcs(5, 6);
+        g.setArcs(6, 1);
+        g.setArcs(6, 3);
+        g.setArcs(6, 5);
+        g.setArcs(6, 7);
+        g.setArcs(7, 3);
+        g.setArcs(7, 4);
+        g.setArcs(7, 6);
+        g.setArcs(8, 1);
+        g.setArcs(8, 2);
+        g.setArcs(8, 3);
+
+        System.out.print("深度优先遍历（递归）：");
+        g.DFSTraverse();
 	}
 }
