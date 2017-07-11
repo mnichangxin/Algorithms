0326ec00191844c6c6afd00bfb64ee28c51f4313
mnichangxin <mnichangxin@163.com>
2017-07-12 00:08:03 +0800

    commit

---
+++ ACPC/1.5/1-1.cpp
@@ -0,0 +1,9 @@
+// 1-1 Æ½¾ùÊý 
+#include <iostream>
+
+int main(int argc,char *argv[]) {
+	int a, b, c;
+	
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%.3f", (a + b + c) / 3.0);
+}
---
+++ ACPC/1.5/1-2.cpp
@@ -0,0 +1,11 @@
+// 1-2 ÎÂ¶È
+#include <iostream>
+
+int main(int argc,char *argv[]) {
+	float f, c;
+	
+	scanf("%f", &f);
+	c = 5 * (f - 32) / 9;
+	
+	printf("%.3f", c);
+}
