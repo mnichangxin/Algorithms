157e78f38cca6f65f2bbfa9684f8287dfb8af6e9
mnichangxin <mnichangxin@163.com>
2019-02-03 23:30:28 +0800

    add leetcode

---
+++ leetcode/1.two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        N = len(nums)
+        for i in range(N - 1):
+            for j in range(i + 1, N):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
