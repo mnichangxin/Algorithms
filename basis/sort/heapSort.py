f9343db996823b574abde3019a1639643863a561
mnichangxin <mnichangxin@163.com>
2019-02-20 15:27:47 +0800

    update heapSort

--- basis/sort/heapSort.py
+++ basis/sort/heapSort.py
@@ -1,13 +1,28 @@
 import sys
 
 
-def adjustHeap(arr, i, lenth):
-    k = arr[i]
-    for i in 
+def adjustHeap(arr, k, n):
+    N = n - 1
+    while 2 * k <= N:
+        j = 2 * k
+        if j < N and arr[j] < arr[j + 1]:
+            j += 1
+        if arr[k] < arr[j]:
+            arr[k], arr[j] = arr[j], arr[k]
+            k = j
+        else:
+            break
+
 
 def heapSort(arr):
-    for i in range(len(arr) // 2 - 1, -1, -1):    
-    
+    n = len(arr) - 1
+    for i in range(n // 2, 0, -1):
+        adjustHeap(arr, i, len(arr))
+    while n > 1:
+        arr[1], arr[n] = arr[n], arr[1]
+        adjustHeap(arr, 1, n)
+        n -= 1
+    return arr[1:]
 
 for line in sys.stdin:
     arr = list(map(int, line.split()))
