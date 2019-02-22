6dbed6eb59f60c6465f45faa60fd4cc40128fd1f
mnichangxin <mnichangxin@163.com>
2019-02-22 15:46:30 +0800

    update leetcode

--- leetcode/2.add-two-numbers.py
+++ leetcode/2.add-two-numbers.py
@@ -12,3 +12,22 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
+        dummyHead = ListNode(0)
+        p = l1
+        q = l2
+        cur = dummyHead
+        carry = 0
+        while p is not None and q is not None:
+            x = p.val if p is not None else 0
+            y = q.val if q is not None else 0
+            sum = (x + y + carry)
+            carry = sum // 10
+            cur.next = ListNode(sum % 10)
+            cur = cur.next
+            if p is not None:
+                p = p.next
+            if q is not None:
+                q = q.next
+        if carry > 0:
+            cur.next = ListNode(carry)
+        return dummyHead.next
