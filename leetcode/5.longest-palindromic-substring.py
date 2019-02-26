4f7d183810952d1edc983cefb5f34eb44e2daa8c
mnichangxin <mnichangxin@163.com>
2019-02-26 16:29:55 +0800

    update leetcode

--- leetcode/5.longest-palindromic-substring.py
+++ leetcode/5.longest-palindromic-substring.py
@@ -1,3 +1,10 @@
 class Solution:
+    def isPalindrome(self, s: 'str') -> 'bool':
+        return s == s[::-1]
+
+    def getPalindrome(self, s: 'str', i: 'int', j: 'int') -> 'str':
+        return self.getPalindrome(s, i, j)
+
     def longestPalindrome(self, s: 'str') -> 'str':
-        
+        n = len(s)
+        return self.getPalindrome(s, 0, 0)
