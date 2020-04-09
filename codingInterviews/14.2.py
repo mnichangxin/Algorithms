7a451a3e57556532d18e0a8de871fec503f0dedf
mnichangxin <mnichangxin@163.com>
2020-04-09 10:02:43 +0800

    Add

---
+++ codingInterviews/14.2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def cuttingRope(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[2] = 1
+
+        for i in range(3, n + 1):
+            for j in range(i):
+                dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j))
+            
+        return dp[n] % 1000000007
